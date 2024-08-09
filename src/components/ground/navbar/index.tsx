d3797fab5b887ca2abf217cf6a43b5bb94e5ad1c
soumit-s <soumit.srim@gmail.com>
2024-08-09 23:32:53 +0530

    Changed design of the diagram navigation bar

--- src/components/ground/navbar/index.tsx
+++ src/components/ground/navbar/index.tsx
@@ -1,16 +1,30 @@
 import Button from "@/components/common/button";
 import LogoMenu from "./LogoMenu";
+import ButtonLink from "@/components/common/button-link";
+import DashboardIcon from "@/components/svg/Dashboard1.svg";
 
 interface Props {
   projectName: string;
 }
 
 const NavigationBar = (props: Props) => (
-  <nav className="p-2 border border-outline rounded-lg flex items-center justify-between bg-primary">
+  <nav className="rounded-lg flex items-center justify-between relative pt-4 p-2">
     <LogoMenu />
-    <span className="py-2 px-4 bg-outline rounded">{props.projectName}</span>
-    <div>
-      <Button variant="hollow" className="uppercase">sign in</Button>
+    <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-start border-t-8 border-secondary -z-10">
+      <span className="py-2 px-4 bg-outline rounded-b outline-2 outline outline-secondary font-handwriting black-shadow bg-secondary">
+        {props.projectName}
+      </span>
+    </div>
+    <div className="flex items-center gap-8 font-handwriting">
+      <a className="flex items-center gap-4 hover:cursor-pointer hover:bg-outline p-2 rounded-md" href="/dashboard">
+        <DashboardIcon className="w-6 h-6 fill-foreground" /> Dashboard
+      </a>
+      <Button
+        variant="filled"
+        className="uppercase font-handwriting black-shadow"
+      >
+        sign in
+      </Button>
     </div>
   </nav>
 );
