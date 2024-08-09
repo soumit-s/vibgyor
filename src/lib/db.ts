66bd8e4838cd6a27702bdc1bfc83c32ffb78b940
soumit-s <soumit.srim@gmail.com>
2024-08-09 21:38:14 +0530

    Created hooks for diagrams, entites and fields.

---
+++ src/lib/db.ts
@@ -0,0 +1,76 @@
+import { Dexie, EntityTable } from "dexie";
+
+export interface Timestamps {
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+export interface Diagram extends Timestamps {
+  id: string;
+  name: string;
+}
+
+export interface Entity extends Timestamps {
+  id: number;
+  name: string;
+  x: number;
+  y: number;
+}
+
+const entitySchema = `
+  ++id, 
+  &name, 
+  x,
+  y,
+  updatedAt, 
+  createdAt
+`;
+
+export interface Field extends Timestamps {
+  id: number;
+  entityId: number;
+  name: string;
+  type: string;
+  isPrimaryKey: boolean;
+  isNotNull: boolean;
+  isUnique: boolean;
+}
+
+const fieldSchema = `
+  ++id, 
+  name, 
+  entityId, 
+  type, 
+  isPrimaryKey, 
+  isNotNull, 
+  isUnique, 
+  updatedAt, 
+  createdAt
+`;
+
+export interface Relations extends Timestamps {
+  id: number;
+  type: "one-to-one" | "one-to-many" | "many-to-many";
+  from: number; // Id of some Field object.
+  to: number; // Id of some Field object.
+}
+
+export type Database = Dexie & {
+  diagrams: EntityTable<Diagram, "id">;
+  entities: EntityTable<Entity, "id">;
+  fields: EntityTable<Field, "id">;
+  relations: EntityTable<Field, "id">;
+};
+
+Dexie.delete("local-db");
+
+const db = new Dexie("local-db") as Database;
+
+db.version(1).stores({
+  diagrams: "++id, name, updatedAt, createdAt",
+  entities: entitySchema,
+  fields: fieldSchema,
+  relations: "++id, type, updatedAt, createdAt",
+});
+
+export const getLocalDb = () => db;
---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,42 @@
+"use client";
+import { useLiveQuery } from "dexie-react-hooks";
+import { getLocalDb } from "../db";
+
+export const useDiagrams = () => {
+  return useLiveQuery(() => getLocalDb().diagrams.toArray())?.sort(
+    (a, b) => b.updatedAt.getTime() - a.updatedAt.getTime()
+  );
+};
+
+export const useDiagramById = (id: string) =>
+  useLiveQuery(
+    () => getLocalDb().diagrams.where("id").equals(id).first(),
+    [id]
+  );
+
+export const useEntites = () => {
+  return useLiveQuery(() => getLocalDb().entities.toArray());
+};
+
+export const useEntity = (entity: number | { id: number }) => {
+  return useLiveQuery(
+    () =>
+      getLocalDb()
+        .entities.where("id")
+        .equals(entity instanceof Object ? entity.id : entity)
+        .first(),
+    [entity]
+  );
+};
+
+export const useEntityFields = (entity: number | { id: number }) => {
+  return useLiveQuery(
+    () =>
+      getLocalDb()
+        .fields.where("entityId")
+        .equals(entity instanceof Object ? entity.id : entity)
+        .toArray(),
+    [entity],
+    []
+  );
+};
