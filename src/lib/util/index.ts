5260f7a7d77d9ba08574762fd6ab0e80e0e7450a
soumit-s <soumit.srim@gmail.com>
2024-08-16 00:19:25 +0530

    Added syncDiagramDataWithEditorSchema function

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,5 +1,5 @@
 import { Parser } from "@dbml/core";
-import { Diagram, Entity, getLocalDb } from "../db";
+import { Diagram, Entity, Field, getLocalDb } from "../db";
 
 export function generateSlugFromName(name: string) {
   return name.replaceAll(/[\s]*/g, "-");
@@ -25,6 +25,14 @@ export async function isDiagramExistsByName(name: string): Promise<boolean> {
   return (await getLocalDb().diagrams.where("name").equals(name).count()) != 0;
 }
 
+export async function getDiagramById(diagramId: string) {
+  return await getLocalDb().diagrams.where("id").equals(diagramId).first();
+}
+
+export async function getDiagramByName(diagramName: string) {
+  return await getLocalDb().diagrams.where("name").equals(diagramName).first();
+}
+
 export async function deleteDiagram(id: string): Promise<void> {
   await getLocalDb().diagrams.delete(id);
 }
@@ -36,11 +44,14 @@ export async function updateDiagramName(
   return !!(await getLocalDb().diagrams.update(diagram, { name: newName }));
 }
 
-export async function createEntity(data: {
-  name: string;
-  x?: number;
-  y?: number;
-}, diagram: Pick<Diagram, 'id'>) {
+export async function createEntity(
+  data: {
+    name: string;
+    x?: number;
+    y?: number;
+  },
+  diagram: Pick<Diagram, "id">
+) {
   const now = new Date();
   return await getLocalDb().entities.add({
     name: data.name,
@@ -50,6 +61,10 @@ export async function createEntity(data: {
     createdAt: now,
     updatedAt: now,
   });
+}
+
+export async function getEntites() {
+  return await getLocalDb().entities.toArray();
 }
 
 export async function getEntityById(id: number): Promise<Entity | undefined> {
@@ -67,21 +82,18 @@ export async function createEntityField(
     name: string;
     type: string;
     isUnique?: boolean;
-    isPrimary?: boolean;
+    isPrimaryKey?: boolean;
     isNotNull?: boolean;
   },
   entity: { id: number } | number
 ) {
-  const now = new Date();
   return await getLocalDb().fields.add({
     name: data.name,
     entityId: entity instanceof Object ? entity.id : entity,
     type: data.type,
     isUnique: data.isUnique ?? false,
     isNotNull: data.isNotNull ?? false,
-    isPrimaryKey: data.isPrimary ?? false,
-    createdAt: now,
-    updatedAt: now,
+    isPrimaryKey: data.isPrimaryKey ?? false,
   });
 }
 
@@ -96,10 +108,10 @@ export async function updateEntityPosition(
   entity: number | Entity,
   position: { x: number; y: number }
 ): Promise<boolean> {
-  return !!await getLocalDb().entities.update(entity, {
+  return !!(await getLocalDb().entities.update(entity, {
     x: position.x,
     y: position.y,
-  });
+  }));
 }
 
 const dbmlParser = new Parser();
@@ -109,5 +121,73 @@ export async function parseDbml(v: string) {
     return dbmlParser.parse(v, "dbml");
   } catch (e) {
     return undefined;
-  };
+  }
+}
+
+export async function syncDiagramDataWithEditorSchema(
+  diagramId: string,
+  dbData: ReturnType<typeof dbmlParser.parse>
+) {
+  const db = getLocalDb();
+  const entites = await getEntites();
+  const schema = dbData.findOrCreateSchema("public");
+
+  const currentEntityNameToEntityIdMap = entites.reduce(
+    (map, e) => map.set(e.name, e.id),
+    new Map<string, number>()
+  );
+  const allTableIds = entites.reduce(
+    (arr, entity) => arr.add(entity.id),
+    new Set<number>()
+  );
+  const updatedTableIds = new Array<number>();
+  const updatedTableData = new Array<(typeof schema.tables)[0]>();
+  const newTables = schema.tables.reduce((set, v) => {
+    const id = currentEntityNameToEntityIdMap.get(v.name);
+    if (id) {
+      updatedTableData.push(v);
+      updatedTableIds.push(id);
+      allTableIds.delete(id);
+    } else set.push(v);
+    return set;
+  }, new Array<(typeof schema.tables)[0]>());
+
+  // Takes care of deleted tables.
+  const deletedTableIds = Array.from(allTableIds);
+  await db.entities.bulkDelete(Array.from(deletedTableIds));
+  await db.fields
+    .where("entityId")
+    .equals(Array.from(deletedTableIds))
+    .delete();
+
+  console.log(updatedTableData);
+
+  // Takes care of the new tables.
+  for (const table of newTables) {
+    const entityId = await createEntity(
+      { name: table.name, x: 600, y: 300 },
+      { id: diagramId }
+    );
+    for (const field of table.fields) {
+      await createEntityField(
+        { name: field.name, type: field.type.type_name },
+        { id: entityId }
+      );
+    }
+  }
+
+  for (const data of updatedTableData) {
+    const entityId = currentEntityNameToEntityIdMap.get(data.name)!;
+    const updatedFields = data.fields.map((f) => {
+      return {
+        entityId,
+        name: f.name,
+        type: f.type.type_name,
+        isPrimaryKey: f.pk,
+        isNotNull: f.not_null,
+        isUnique: f.unique,
+      };
+    });
+    await getLocalDb().fields.bulkPut(updatedFields);
+  }
 }
