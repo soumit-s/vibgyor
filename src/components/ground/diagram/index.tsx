77950ee0906c7298ab1f41f7d6c8848e4f31fc9d
soumit-s <soumit.srim@gmail.com>
2024-08-16 00:21:52 +0530

    Added dyanmic import for Entity component. Only shows entities belonging to the current diagram

--- src/components/ground/diagram/index.tsx
+++ src/components/ground/diagram/index.tsx
@@ -4,16 +4,23 @@ import { useEffect, useState } from "react";
 import { Vector2d } from "konva/lib/types";
 import { KonvaEventObject } from "konva/lib/Node";
 import DiagramGrid from "../diagram-grid";
-import Entity from "../entity";
 import { useEntites } from "@/lib/hooks";
+import dynamic from "next/dynamic";
+const Entity = dynamic(() => import("@/components/ground/entity"), {
+  ssr: false,
+});
 
-const Diagram = () => {
+interface DiagramProps {
+  diagramId: string;
+}
+
+const Diagram = ({ diagramId }: DiagramProps) => {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
   const [stagePos, setStagePos] = useState<Vector2d>({ x: 0, y: 0 });
-  const entities = useEntites();
+  const entities = useEntites(diagramId);
 
   const handleWindowResize = () => {
     setWindowSize({ width: window.innerWidth, height: window.innerHeight });
@@ -36,7 +43,7 @@ const Diagram = () => {
       draggable
       onDragEnd={(e) => setStagePos(e.currentTarget.position())}
       onWheel={handleZoom}
-      >
+    >
       <DiagramGrid stagePos={stagePos} windowSize={windowSize} />
       <Layer>
         {entities &&
